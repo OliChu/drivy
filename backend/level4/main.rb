fd824b899464d69e6d57846c25bcf0c9f2e71680
Olivier Chuiton <olivier.chuiton@gmail.com>
2018-06-26 22:16:06 +0200

    solving level 4

---
+++ backend/level4/main.rb
@@ -0,0 +1,78 @@
+require 'json'
+require 'date'
+
+def write_output_json(data, output_filepath)
+  File.open(output_filepath, 'wb') do |file|
+    file.write(JSON.pretty_generate(data))
+  end
+end
+
+def weighting_coefficient(number_of_days)
+  coefficient = 1
+  if 1 < number_of_days && number_of_days <= 4
+    coefficient -= 0.10
+  elsif 4 < number_of_days && number_of_days <= 10
+    coefficient -= 0.30
+  elsif number_of_days > 10
+    coefficient -= 0.5
+  end
+  return coefficient
+end
+
+def calcul_rental_price(number_of_days, price_per_day)
+  (1..number_of_days).to_a.reduce(0){ |sum, day| sum + price_per_day * weighting_coefficient(day) }
+end
+
+def calcul_commission_fees(price, number_of_days)
+  commission = (price * 0.30).to_i
+  insurance_fee = (commission * 0.50).to_i
+  assistance_fee = 100 * number_of_days
+  drivy_fee = commission - (insurance_fee + assistance_fee)
+  {
+    "insurance_fee": insurance_fee,
+    "assistance_fee": assistance_fee,
+    "drivy_fee": drivy_fee
+  }
+end
+
+# OPEN JSON AND PARSE
+filepath = 'data/input.json'
+serialized_input = File.read(filepath)
+input = JSON.parse(serialized_input)
+
+# CREATE VARIABLES
+cars = input['cars']
+rentals = input['rentals']
+prices = { 'rentals' => []}
+
+# SETTING PRICES
+rentals.each do |rental|
+  number_of_days = (DateTime.strptime(rental['end_date'], '%Y-%m-%d') - DateTime.strptime(rental['start_date'], '%Y-%m-%d') + 1).to_i
+  car = cars.detect{ |car| car['id'] == rental['car_id'] }
+  price = (calcul_rental_price(number_of_days, car['price_per_day'].to_i) + (rental['distance'].to_i * car['price_per_km'].to_i)).to_i
+  commission = calcul_commission_fees(price, number_of_days)
+  actions = [
+    {
+      "who": "driver",
+      "type": "debit",
+      "amount": price
+    },
+    {
+      "who": "owner",
+      "type": "credit",
+      "amount": (price * 0.7).to_i
+    }]
+    commission.each do |type, amount|
+      action = {
+        "who": type.to_s.gsub('_fee', ''),
+        "type": "credit",
+        "amount": amount
+      }
+      actions << action
+    end
+  prices['rentals'] << { id: rental['id'], actions: actions }
+end
+
+# WRITE JSON
+output_filepath = 'data/output.json'
+write_output_json(prices, output_filepath)
